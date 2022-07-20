a0032d77fd6005c02889643e94ceeec7be3938fb
James Whale <james@james-whale.com>
2022-07-20 14:51:09 +0100

    add pip dependency check and install to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,10 @@
 # https://github.com/pi-hole/pi-hole/blob/master/automated%20install/basic-install.sh
 set -e
 
-#TEST_PACKAGES="cowsay lolcat"
-
 DETECTED_OS=$(grep '^ID=' /etc/os-release | cut -d '=' -f2 | tr -d '"')
 DETECTED_VERSION=$(grep VERSION_ID /etc/os-release | cut -d '=' -f2 | tr -d '"')
-APT_DEPENDENCIES="$TEST_PACKAGES curl git python3"
+APT_DEPENDENCIES="curl git python3"
+PIP_DEPENDENCIES="ansible"
 
 COL_NC='\e[0m'
 COL_LIGHT_GREEN='\e[1;32m'
@@ -35,18 +34,18 @@ is_command(){
 check_apt_dependencies(){
     installArray=""
     for i in "$@"; do
-        spinner_text "Checking for ${i}..." &
+        spinner_text "${PACKAGE_MANAGER}: Checking for ${i}..." &
         SPIN_PID="$!"
         trap "kill -9 $SPIN_PID" `seq 0 15`
         set +e
         if dpkg-query -W -f='${Status}' "${i}" 2>/dev/null | grep "ok installed" &>/dev/null; then
             rc="$?"
             kill -9 $SPIN_PID 2>/dev/null
-            printf "%b[ %b ] Checked for %s\\n" "${OVERWRITE}" "${SUCCESS}" "${i}"
+            printf "%b[ %b ] APT: Checked for %s\\n" "${OVERWRITE}" "${SUCCESS}" "${i}"
         else
             rc="$?"
             kill -9 $SPIN_PID 2>/dev/null
-            printf "%b[ %b ] Checked for %s (will be installed)\\n" "${OVERWRITE}" "${FAILURE}" "${i}"
+            printf "%b[ %b ] APT: Checked for %s (will be installed)\\n" "${OVERWRITE}" "${FAILURE}" "${i}"
             installArray="$installArray$i "
         fi
         set -e
@@ -56,25 +55,25 @@ check_apt_dependencies(){
 install_apt_dependencies(){
     if [[ "${#installArray[@]}" -gt 0 ]]; then
         for package in $installArray; do
-            spinner_text "Processing install(s) for: $package..." &
+            spinner_text "Processing ${PACKAGE_MANAGER} install(s) for: $package..." &
             SPIN_PID="$!"
             trap "kill -9 $SPIN_PID" `seq 0 15`
             set +e
             if "${PACKAGE_MANAGER}" install -y "$package" &>/dev/null; then
                 rc="$?"
                 kill -9 $SPIN_PID 2>/dev/null
-                printf "%b[ %b ] Processed install(s) for: %s\\n" "${OVERWRITE}" "${SUCCESS}" "$package"
+                printf "%b[ %b ] Processed ${PACKAGE_MANAGER} install(s) for: %s\\n" "${OVERWRITE}" "${SUCCESS}" "$package"
             else
                 rc="$?"
                 kill -9 $SPIN_PID 2>/dev/null
-                printf "%b[ %b ] Processed install(s) for: %s\\n" "${OVERWRITE}" "${FAILURE}" "$package"
+                printf "%b[ %b ] Processed ${PACKAGE_MANAGER} install(s) for: %s\\n" "${OVERWRITE}" "${FAILURE}" "$package"
             fi
             set -e
         done
     fi
 }
 
-install_pip_dependency(){
+install_pip(){
     if [ ! -f "/tmp/get-pip.py" ]; then
         spinner_text "Downloading pip bootstrapping script..." &
         SPIN_PID="$!"
@@ -108,26 +107,69 @@ install_pip_dependency(){
     set -e
 }
 
-check_pip_dependency(){
-    spinner_text "Checking for pip..." & 
+check_pip(){
+    spinner_text "PYTHON: Checking for pip..." & 
     SPIN_PID="$!"
     trap "kill -9 $SPIN_PID" `seq 0 15`
     set +e
     if python3 -m pip -V 2&>/dev/null; then 
         rc="$?"
         kill -9 $SPIN_PID 2>/dev/null
-        printf "%b[ %b ] Checked for pip\\n" "${OVERWRITE}" "${SUCCESS}"
+        printf "%b[ %b ] PYTHON: Checked for pip\\n" "${OVERWRITE}" "${SUCCESS}"
     else
         rc="$?"
         kill -9 $SPIN_PID 2>/dev/null
-        printf "%b[ %b ] Checked for pip (will be installed)\\n" "${OVERWRITE}" "${FAILURE}"
-        install_pip_dependency
+        printf "%b[ %b ] PYTHON: Checked for pip (will be installed)\\n" "${OVERWRITE}" "${FAILURE}"
+        install_pip
     fi
     set -e
+}
+
+check_pip_dependencies(){
+    installArray=""
+    for i in "$@"; do
+        spinner_text "PIP: Checking for ${i}..." &
+        SPIN_PID="$!"
+        trap "kill -9 $SPIN_PID" `seq 0 15`
+        set +e
+        if python3 -m pip list | grep "$i " &>/dev/null; then
+            rc="$?"
+            kill -9 $SPIN_PID 2>/dev/null
+            printf "%b[ %b ] PIP: Checked for %s\\n" "${OVERWRITE}" "${SUCCESS}" "${i}"
+        else
+            rc="$?"
+            kill -9 $SPIN_PID 2>/dev/null
+            printf "%b[ %b ] PIP: Checked for %s (will be installed)\\n" "${OVERWRITE}" "${FAILURE}" "${i}"
+            installArray="$installArray$i "
+        fi
+        set -e
+    done
+}
+
+install_pip_dependencies(){
+    if [[ "${#installArray[@]}" -gt 0 ]]; then
+        for package in $installArray; do
+            spinner_text "Processing pip install(s) for: $package..." &
+            SPIN_PID="$!"
+            trap "kill -9 $SPIN_PID" `seq 0 15`
+            set +e
+            if python3 -m pip install --user "$package" &>/dev/null; then
+                rc="$?"
+                kill -9 $SPIN_PID 2>/dev/null
+                printf "%b[ %b ] Processed pip install(s) for: %s\\n" "${OVERWRITE}" "${SUCCESS}" "$package"
+            else
+                rc="$?"
+                kill -9 $SPIN_PID 2>/dev/null
+                printf "%b[ %b ] Processed pip install(s) for: %s\\n" "${OVERWRITE}" "${FAILURE}" "$package"
+            fi
+            set -e
+        done
+    fi
 }
 
 [ "$(is_command apt)" ] || PACKAGE_MANAGER="apt"
 check_apt_dependencies $APT_DEPENDENCIES
 install_apt_dependencies
-check_pip_dependency
-#"${PACKAGE_MANAGER}" remove -y $TEST_PACKAGES
+check_pip
+check_pip_dependencies $PIP_DEPENDENCIES
+install_pip_dependencies
