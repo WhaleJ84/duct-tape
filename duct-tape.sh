e9c8f7e5582f136360a505c55ad158eb47a54fe1
James Whale <james@james-whale.com>
2024-11-17 17:06:42 +0000

    docs:add full-stop to emphasise end of sentence in requirements usage

--- duct-tape.sh
+++ duct-tape.sh
@@ -81,7 +81,7 @@ the system and pull down desired runbooks from Git repository.
 OPTIONS:
     -b      Bypass OS check. Run script on untested systems
     -d      Perform dry run. Do not make any modifications
-    -f REQ. Force the program to redownload requirements where possible
+    -f REQ. Force the program to redownload requirements where possible.
 	    Specific requirements can be passed in a space separated 
 	    string (e.g. "req1 req2 req3"). Must be specified last
     -h      Display this message and exit
